56aded5d4eff44a447c531c92b7bb0982f152fa1
Vitor Moreira Araujo <vitormoreira.araujo@gmail.com>
2023-06-29 14:23:02 -0300

    Mudando a fonte

--- Revisao/Form1.Designer.cs
+++ Revisao/Form1.Designer.cs
@@ -56,11 +56,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Nirmala UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(252, 12);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 21);
+            this.label1.Size = new System.Drawing.Size(57, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Pessoa";
             // 
@@ -222,7 +222,7 @@
             // 
             // btnPesquisar
             // 
-            this.btnPesquisar.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnPesquisar.BackColor = System.Drawing.SystemColors.Control;
             this.btnPesquisar.Image = global::Revisao.Properties.Resources.search_149852;
             this.btnPesquisar.Location = new System.Drawing.Point(343, 43);
             this.btnPesquisar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
